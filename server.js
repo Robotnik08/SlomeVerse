e4fe3c0ee6fedc760ec5757a8bb9cc67cf20671e
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-05-07 15:50:10 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const port = 80;
+const { Server } = require("socket.io");
+let io = null;
+app.use(express.static('public'));
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
+io = new Server(server);
+
